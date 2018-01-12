52f32f05d46f03d227593cd16669bda1453fc4a7
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-01-12 15:05:19 +0100

    Rebulid database structure, add new relationships

---
+++ database/migrations/2018_01_12_135140_create_groups_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('administrator_id');
+            $table->integer('icon_id')->unsigned();
+            $table->timestamps();
+        });
+        Schema::table('groups', function (Blueprint $table) {
+            $table->foreign('icon_id')
+            ->references('id')
+            ->on('icons');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
---
+++ database/migrations/2018_01_12_135551_create_posts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('rating');
+            $table->integer('user_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->timestamps();
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+        });
+        Schema::table('posts', function (Blueprint $table) {
+            $table->foreign('group_id')
+            ->references('id')
+            ->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
