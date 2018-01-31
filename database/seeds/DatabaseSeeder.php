cc56fe5b6d6d5ad76cbaf74a34c49193714ce07a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-01-31 16:58:07 +0100

    Add roles seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
       $this->call([
         UsersTableSeeder::class,
         IconsTableSeeder::class,
+        RolesTableSeeder::class,
         GroupsTableSeeder::class,
         PostsTableSeeder::class
       ]);
