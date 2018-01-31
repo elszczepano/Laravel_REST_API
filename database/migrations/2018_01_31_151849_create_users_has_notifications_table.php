7e15ba02b3d0d6150cf7c19020ed3103fbaa757c
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-01-31 16:23:56 +0100

    Add notifications table and relationship n:m

---
+++ database/migrations/2018_01_31_151849_create_users_has_notifications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersHasNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_has_notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('notification_id')->unsigned();
+            $table->timestamps();
+        });
+        Schema::table('users_has_notifications', function (Blueprint $table) {
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users');
+        });
+        Schema::table('users_has_notifications', function (Blueprint $table) {
+            $table->foreign('notification_id')
+            ->references('id')
+            ->on('notifications');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_has_notifications');
+    }
+}
