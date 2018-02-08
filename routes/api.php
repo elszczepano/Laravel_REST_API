2a203efe1781b911a5ca6519f525f00f0310d928
elszczepano <szczepaniak.dominik98@gmail.com>
2018-02-08 14:50:05 +0100

    Add login and register routes

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use Illuminate\Http\Request;
 use App\User;
 
 Route::get('user', 'UserController@index');
-Route::get('user/{id}', 'UserController@show');
+Route::get('user/{user}', 'UserController@show');
 Route::post('user', 'UserController@store');
-Route::put('user/{id}', 'UserController@update');
-Route::delete('user/{id}', 'UserController@delete');
+Route::put('user/{user}', 'UserController@update');
+Route::delete('user/{user}', 'UserController@delete');
+
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::post('login', 'Auth\LoginController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
