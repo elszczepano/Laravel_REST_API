8212b0d329bf3a6d78d7db26c34ed3df83dc1517
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-08 16:52:53 +0100

    Add required paths

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
