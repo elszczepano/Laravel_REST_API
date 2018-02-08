0dc5f88865bad139713bc588f76a0ba7fc96c522
elszczepano <szczepaniak.dominik98@gmail.com>
2018-02-08 14:53:22 +0100

    Generating token

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,18 +9,24 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
+
+    public function register(Request $request) {
+
+    $this->validator($request->all())->validate();
+
+    event(new Registered($user = $this->create($request->all())));
+
+    $this->guard()->login($user);
+
+    return $this->registered($request, $user)
+              ?: redirect($this->redirectPath());
+    }
+    protected function registered(Request $request, $user) {
+      $user->generateToken();
+
+      return response()->json(['data' => $user->toArray()], 201);
+    }
 
     /**
      * Where to redirect users after registration.
