191e5813a1bb7e208db70210c0635e3957cdafa6
elszczepano <szczepaniak.dominik98@gmail.com>
2018-02-09 10:10:44 +0100

    Route group instead of simple paths

--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,25 @@
 use Illuminate\Http\Request;
 use App\User;
 
-Route::get('user', 'UserController@index');
-Route::get('user/{user}', 'UserController@show');
-Route::post('user', 'UserController@store');
-Route::put('user/{user}', 'UserController@update');
-Route::delete('user/{user}', 'UserController@delete');
+
+Route::group(['middleware' => 'auth:api'], function() {
+  Route::get('user', 'UserController@index');
+  Route::get('user/{user}', 'UserController@show');
+  Route::post('user', 'UserController@store');
+  Route::put('user/{user}', 'UserController@update');
+  Route::delete('user/{user}', 'UserController@delete');
+});
 
 Route::post('register', 'Auth\RegisterController@register');
 
 Route::middleware('auth:api')
-    ->get('/user', function (Request $request) {
-        return $request->user();
-    });
+->get('/user', function (Request $request) {
+  return $request->user();
+});
 
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
