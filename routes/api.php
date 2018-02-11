45f2348bd6ebf0c8a27ec8a1e69f047c28660f54
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-11 17:31:55 +0100

    Add routes to controllers

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,30 @@ Route::group(['middleware' => 'auth:api'], function() {
   Route::put('post/{post}', 'PostController@update');
   Route::delete('post/{post}', 'PostController@delete');
 
+  Route::get('icon', 'IconController@index');
+  Route::get('icon/{icon}', 'IconController@show');
+  Route::post('icon', 'IconController@store');
+  Route::put('icon/{icon}', 'IconController@update');
+  Route::delete('icon/{icon}', 'IconController@delete');
+
+  Route::get('role', 'RoleController@index');
+  Route::get('role/{role}', 'RoleController@show');
+  Route::post('role', 'RoleController@store');
+  Route::put('role/{role}', 'RoleController@update');
+  Route::delete('role/{role}', 'RoleController@delete');
+
+  Route::get('notificataion', 'NotificationController@index');
+  Route::get('notificataion/{notificataion}', 'NotificationController@show');
+  Route::post('notificataion', 'NotificationController@store');
+  Route::put('notificataion/{notificataion}', 'NotificationController@update');
+  Route::delete('notificataion/{notificataion}', 'NotificationController@delete');
+
+  Route::get('comment', 'CommentController@index');
+  Route::get('comment/{comment}', 'CommentController@show');
+  Route::post('comment', 'CommentController@store');
+  Route::put('comment/{comment}', 'CommentController@update');
+  Route::delete('comment/{comment}', 'CommentController@delete');
+
 });
 
 Route::post('register', 'Auth\RegisterController@register');
