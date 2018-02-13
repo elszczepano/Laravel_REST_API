a2436700a3f7bd4df3aba1f8ad2e4d775589677a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-13 17:19:55 +0100

    Add pivot relationship

--- app/Group.php
+++ app/Group.php
@@ -8,4 +8,8 @@ class Group extends Model
 {
     protected $fillable = ['name', 'description', 'icon_id'];
     protected $hidden = ['id'];
+
+    public function users() {
+      return $this->belongsToMany(User::class, 'users_has_groups', 'user_id', 'group_id')->withTimestamps();
+    }
 }
