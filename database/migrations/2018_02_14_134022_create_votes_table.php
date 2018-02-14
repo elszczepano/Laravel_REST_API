c0efb56e534e0bf4cfde933ae2ba908058fb4046
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-14 15:01:58 +0100

    Create votes table

---
+++ database/migrations/2018_02_14_134022_create_votes_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('post_id')->unsigned();
+            $table->boolean('voted')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('votes', function (Blueprint $table) {
+          $table->foreign('user_id')
+          ->references('id')
+          ->on('users');
+        });
+        Schema::table('votes', function (Blueprint $table) {
+          $table->foreign('post_id')
+          ->references('id')
+          ->on('post');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
