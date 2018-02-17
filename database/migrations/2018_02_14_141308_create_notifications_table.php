d477c8c31d249a91c936af24b959a291584a6cd7
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-17 16:44:55 +0100

    Move fields from redundant table

--- database/migrations/2018_02_14_141308_create_notifications_table.php
+++ database/migrations/2018_02_14_141308_create_notifications_table.php
@@ -16,7 +16,11 @@ class CreateNotificationsTable extends Migration
     Schema::create('notifications', function (Blueprint $table) {
       $table->increments('id');
       $table->string('content');
+      $table->integer('user_id')->unsigned();
+      $table->boolean('read');
       $table->timestamps();
+
+      $table->foreign('user_id')->references('id')->on('users');
     });
   }
 
