b63c251496e45631a97dd5f9b06fa19977069fc9
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-17 16:46:08 +0100

    Add VoteController with methods

---
+++ app/Http/Controllers/VoteController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Vote;
+use App\Post;
+use App\User;
+
+class VoteController extends Controller
+{
+    public function index()
+    {
+        return Vote::all();
+    }
+
+
+    public function postVotes(Post $post)
+    {
+      return $post->vote()->get();
+    }
+
+
+    public function userVotes(User $user)
+    {
+      return $user->vote()->get();
+    }
+
+
+    public function store(Request $request)
+    {
+      $vote = Vote::create($request->all());
+
+      return response()->json($vote, 201);
+    }
+
+
+    public function show(Vote $vote)
+    {
+      return response()->json($vote);
+    }
+
+
+    public function update(Request $request, Vote $vote)
+    {
+      $vote->update($request->all());
+
+      return response()->json($vote);
+    }
+
+
+    public function destroy(Vote $vote)
+    {
+      $vote->delete();
+
+      return response()->json(null, 204);
+    }
+}
