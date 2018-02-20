f540610ffda4a73cba05359a8a66ba4a2920ca61
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-20 12:09:11 +0100

    Remove comments

--- app/Repositories/IconRepositoryEloquent.php
+++ app/Repositories/IconRepositoryEloquent.php
@@ -8,31 +8,16 @@ use App\Repositories\IconRepository;
 use App\Entities\Icon;
 use App\Validators\IconValidator;
 
-/**
- * Class IconRepositoryEloquent.
- *
- * @package namespace App\Repositories;
- */
 class IconRepositoryEloquent extends BaseRepository implements IconRepository
 {
-    /**
-     * Specify Model class name
-     *
-     * @return string
-     */
     public function model()
     {
         return Icon::class;
     }
 
-    
-
-    /**
-     * Boot up the repository, pushing criteria
-     */
     public function boot()
     {
         $this->pushCriteria(app(RequestCriteria::class));
     }
-    
+
 }
