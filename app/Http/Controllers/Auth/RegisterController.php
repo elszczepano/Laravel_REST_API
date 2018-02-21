59fda5919abab77ad584c83a5214683f8276ffd9
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-21 13:01:13 +0100

    Restore default login and register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,42 +1,28 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
-
-use App\Entities\User;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
-
 class RegisterController extends Controller
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
     use RegistersUsers;
-
-    public function register(Request $request) {
-
-    $this->validator($request->all())->validate();
-
-    event(new Registered($user = $this->create($request->all())));
-
-    $this->guard()->login($user);
-
-    return $this->registered($request, $user)
-              ?: redirect($this->redirectPath());
-    }
-    protected function registered(Request $request, $user) {
-      $user->generateToken();
-
-      return response()->json(['data' => $user->toArray()], 201);
-    }
-
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
     protected $redirectTo = '/home';
-
     /**
      * Create a new controller instance.
      *
@@ -46,7 +32,6 @@ class RegisterController extends Controller
     {
         $this->middleware('guest');
     }
-
     /**
      * Get a validator for an incoming registration request.
      *
@@ -61,12 +46,11 @@ class RegisterController extends Controller
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
-
     /**
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Entities\User
+     * @return \App\User
      */
     protected function create(array $data)
     {
