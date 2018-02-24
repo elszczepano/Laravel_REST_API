5a9214938787e390c1330ea1f4c4de3f7726f854
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-02-24 14:48:39 +0100

    Create exception handlers

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,67 +5,51 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
   protected function unauthenticated($request, AuthenticationException $exception)
   {
-      return response()->json(['error' => 'Unauthenticated'], 401);
+    return response()->json(['error' => 'Unauthenticated'], 401);
   }
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+  protected $dontReport = [];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
+  protected $dontFlash = [
+    'password',
+    'password_confirmation',
+  ];
+
+  public function report(Exception $exception)
+  {
+    parent::report($exception);
+  }
+
+
+  public function render($request, Exception $exception)
+  {
+    if ($exception instanceof ModelNotFoundException) {
+      return response()->json([
+        'success' => false,
+        'error' => 'Resource not found'
+      ], 404);
+    }
+    if ($exception instanceof MethodNotAllowedHttpException) {
+      return response()->json( [
+        'success' => false,
+        'message' => 'Method is not allowed for the requested route'
+      ], 405 );
+    }
+    if ($exception instanceof NotFoundHttpException) {
+      return response()->json( [
+        'success' => false,
+        'message' => 'Resource not found'
+      ], 404 );
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-      if ($exception instanceof ModelNotFoundException) {
-        return response()->json([
-            'error' => 'Resource not found'
-        ], 404);
-      }
-
-      return parent::render($request, $exception);
-    }
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
+    return parent::render($request, $exception);
+  }
 }
