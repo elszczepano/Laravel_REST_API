06b61a9dfdded7ec6e4a97fae4c866ae9c57136d
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-10 18:14:51 +0100

    Add join requests validator

---
+++ app/Validators/JoinRequestsValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Validators;
+
+use \Prettus\Validator\Contracts\ValidatorInterface;
+use \Prettus\Validator\LaravelValidator;
+
+class JoinRequestsValidator extends LaravelValidator
+{
+  protected $rules = [
+      ValidatorInterface::RULE_CREATE => [
+        'user_id' => 'required|exists:users,id|integer',
+        'group_id' => 'required|exists:groups,id|integer',
+        'approved' => 'boolean'
+      ],
+      ValidatorInterface::RULE_UPDATE => [
+        'user_id' => 'exists:users,id|integer',
+        'group_id' => 'exists:groups,id|integer',
+        'role_id' => 'boolean'
+      ],
+  ];
+}
