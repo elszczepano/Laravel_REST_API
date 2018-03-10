7bb5d6313e8ec7ce1bb9345eaa5b0971a2d2e92b
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-10 17:15:25 +0100

    Create join request entity

---
+++ app/Entities/JoinRequests.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class JoinRequests extends Model implements Transformable
+{
+    use SoftDeletes;
+    use TransformableTrait;
+
+    protected $fillable = ['user_id', 'group_id', 'approved'];
+    protected $hidden = ['id'];
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+    public function group()
+    {
+      return $this->belongsTo(Group::class);
+    }
+}
