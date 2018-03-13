29afd30032b158a5ed46b5c07951d54f784b77ad
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-13 18:01:49 +0100

    Add faker with user avatar

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -18,6 +18,7 @@ class UserTableSeeder extends Seeder
         $user -> surname = ('Doe');
         $user -> email = ('johndoe@example.com');
         $user -> birth_date = $faker->dateTime;
+        $user -> avatar = $faker->imageUrl(400, 300);
         $user -> password = bcrypt('secret');
         $user -> save();
 
@@ -26,6 +27,7 @@ class UserTableSeeder extends Seeder
         $user -> surname = ('Doe');
         $user -> email = ('jackdoe@example.com');
         $user -> birth_date = $faker->dateTime;
+        $user -> avatar = $faker->imageUrl(400, 300);
         $user -> password = bcrypt('secret');
         $user -> save();
     }
