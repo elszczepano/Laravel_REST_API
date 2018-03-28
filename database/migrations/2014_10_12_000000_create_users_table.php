9ce9be6bfe5d471cac8b0bfcdc1c098daa2a83c0
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-03-28 18:17:36 +0200

    Change default photo URL

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
       $table->increments('id');
       $table->string('name');
       $table->string('surname')->nullable();
-      $table->string('avatar')->nullable()->default('https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png');
+      $table->string('avatar')->nullable()->default('https://imgur.com/a/P2u4d');
       $table->string('email')->unique();
       $table->date('birth_date')->nullable();
       $table->string('password');
