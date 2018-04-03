38b718dfa15736c758118bf6f545a562d8128462
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-03 20:41:57 +0200

    Add icon set

--- database/seeds/IconTableSeeder.php
+++ database/seeds/IconTableSeeder.php
@@ -5,15 +5,87 @@ use App\Entities\Icon;
 
 class IconTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-      $icon = new Icon();
-      $icon -> name = ('envelope');
-      $icon -> save();
+      $icons = [
+        'address-book',
+        'address-card',
+        'envelope',
+        'microchip',
+        'shower',
+        'thermometer',
+        'anchor',
+        'area-chart',
+        'at',
+        'bank',
+        'battery',
+        'bell',
+        'bicycle',
+        'birthday-cake',
+        'bolt',
+        'bomb',
+        'book',
+        'bookmark',
+        'briefcase',
+        'bug',
+        'calendar',
+        'car',
+        'check',
+        'cloud',
+        'coffee',
+        'cogs',
+        'code',
+        'commenting',
+        'cutlery',
+        'tachometer',
+        'database',
+        'diamond',
+        'feed',
+        'flag',
+        'glass',
+        'gift',
+        'globe',
+        'group',
+        'heart',
+        'home',
+        'key',
+        'laptop',
+        'leaf',
+        'map',
+        'map-marker',
+        'money',
+        'music',
+        'pencil',
+        'paw',
+        'phone',
+        'plane',
+        'plug',
+        'rocket',
+        'search',
+        'snowflake-o',
+        'star',
+        'support',
+        'suitcase',
+        'tag',
+        'tint',
+        'trash',
+        'tree',
+        'trophy',
+        'truck',
+        'umbrella',
+        'wifi',
+        'wrench',
+        'video-camera',
+        'motorcycle',
+        'btc',
+        'dollar',
+        'euro'
+      ];
+
+      foreach ($icons as $name) {
+        $icon = new Icon();
+        $icon -> name = ($name);
+        $icon -> save();
+      }
     }
 }
