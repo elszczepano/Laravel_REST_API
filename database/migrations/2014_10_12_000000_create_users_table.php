6de63d3c55d38eecf9bd767cdb4ab047cb312656
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-04-22 14:56:10 +0200

    Remove 'nullable' from user avatar - user has default avatar.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
       $table->increments('id');
       $table->string('name');
       $table->string('surname')->nullable();
-      $table->string('avatar')->nullable()->default('avatar.png');
+      $table->string('avatar')->default('avatar.png');
       $table->string('email')->unique();
       $table->date('birth_date')->nullable();
       $table->string('description')->nullable();
